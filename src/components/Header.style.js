e39e4702ef27f28725d0620c6ec54cc315d5d228
JBlin1819 <jaclyn.blinstrubas@gmail.com>
2021-07-03 13:16:10 -0400

    started styled components

---
+++ src/components/Header.style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+
+export const Header = styled.header`
+    margin: auto;
+    width: 100vw;
+    height: 15vh;
+    background-color: #86D0C2;
+    color: #272727;
+`;
